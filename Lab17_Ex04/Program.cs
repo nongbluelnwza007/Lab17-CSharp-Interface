c60ddf4472215524bd0536de70d28165e8507532
nongbluelnwza007 <booblueboyy@gmail.com>
2025-03-29 17:21:22 +0700

    add

---
+++ Lab17_Ex04/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+interface ISpeakable
+{
+    void Speak();
+}
+
+abstract class Animal : IMovable, ISpeakable
+{
+    public abstract void Move();
+    public abstract void Speak();  // Added Speak() method
+}
+
+class Dog : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Dog moves by running on the ground");
+    }
+    public override void Speak()
+    {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+class Fish : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Fish moves by swimming in the water");
+    }
+    public override void Speak()
+    {
+        Console.WriteLine("Fish makes bubbles");
+    }
+}
+
+class Bird : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Bird moves by flying in the air");
+    }
+    public override void Speak()
+    {
+        Console.WriteLine("Bird chirps");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal[] animals = new Animal[3];
+        animals[0] = new Dog();
+        animals[1] = new Bird();
+        animals[2] = new Fish();
+
+        foreach (var animal in animals)
+        {
+            animal.Move();
+            animal.Speak();  // Now we can call Speak() on each animal
+        }
+    }
+}
---
+++ Lab17_Ex06/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+interface IRemoteControl
+{
+    void TurnOn();
+    void TurnOff();
+    void ChannelUp();
+    void ChannelDown();
+}
+
+abstract class PowerAppliance
+{
+    public bool PowerStatus;
+    public int Wattage;
+}
+
+class Television : PowerAppliance
+{
+    public int Channel { get; set; }
+}
+
+class Lamp : PowerAppliance
+{
+}
+
+class SonyTV : Television, IRemoteControl
+{
+    public void TurnOn() { Console.WriteLine("TV Turn on"); PowerStatus = true; }
+    public void TurnOff() { Console.WriteLine("TV Turn off"); PowerStatus = false; }
+    public void ChannelUp() { Console.WriteLine("TV Channel up"); }
+    public void ChannelDown() { Console.WriteLine("TV Channel down"); }
+}
+
+class DesktopLamp : Lamp, IRemoteControl
+{
+    public void TurnOn() { Console.WriteLine("Lamp Turn on"); PowerStatus = true; }
+    public void TurnOff() { Console.WriteLine("Lamp Turn off"); PowerStatus = false; }
+    public void ChannelUp() { Console.WriteLine("Lamp cannot change channel"); }
+    public void ChannelDown() { Console.WriteLine("Lamp cannot change channel"); }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IRemoteControl myTV = new SonyTV();  // FIXED: Use IRemoteControl
+        myTV.TurnOn();
+        myTV.ChannelUp();
+        myTV.ChannelDown();
+        myTV.TurnOff();
+
+        IRemoteControl myLamp = new DesktopLamp(); // FIXED: Use IRemoteControl
+        myLamp.TurnOn();
+        myLamp.ChannelUp();
+        myLamp.ChannelDown();
+        myLamp.TurnOff();
+    }
+}
